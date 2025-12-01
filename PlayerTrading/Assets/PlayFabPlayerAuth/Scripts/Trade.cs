a988e351c00bea5665828e7cf6306b11f5c288d6
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-12-01 10:48:17 -0700

    write out Trade.cs
    - add 'using System.Collections.Generic;" to headers to use 'List<>'
    - add variables
    - make an instance
    - make a unity event
    - make functions: OnLoggedIn, GetInvetory, GetCatalog

--- PlayerTrading/Assets/PlayFabPlayerAuth/Scripts/Trade.cs
+++ PlayerTrading/Assets/PlayFabPlayerAuth/Scripts/Trade.cs
@@ -1,7 +1,49 @@
 using UnityEngine;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.Events;
 
 public class Trade : MonoBehaviour
 {
+    public GameObject tradeCanvas;
+    public TextMeshProUGUI inventoryText;
+
+    [HideInInspector]
+    public List<ItemInstance> inventory;
+    
+    [HideInInspector]
+    public List<CatalogItem> catalog;
+
+    // OnRefreshUI will be called when we want to update the updated info
+    public UnityEvent onRefreshUI;
+
+    
+    // Create an instance of the script to use throuhgout project
+    public static Trade instance;
+    void Awake() { instance = this; }
+
+    public void OnLoggedIn() 
+    {
+        tradeCanvas.SetActive(true);
+
+        if(onRefreshUI != null)
+        {
+            onRefreshUI.Invoke();
+        }
+    } 
+
+    //GetInventory & GetCatalog are called when we want to refresh UI
+    public void GetInventory()
+    {
+
+    }
+
+    public void GetCatalog()
+    {
+
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

59db8a77486f51a5b1b046f5273a76048d40db43
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-12-03 10:54:59 -0700

    Fix the item trading UI
    Assign wood and metal to offering & requesting items
    
    Add OfferingItems and Requesting items to their respective lists in the inspectorwindow.
    
    Add ResetItemValues

--- PlayerTrading/Assets/PlayFabPlayerAuth/Scripts/TradeOffers.cs
+++ PlayerTrading/Assets/PlayFabPlayerAuth/Scripts/TradeOffers.cs
@@ -1,16 +1,54 @@
 using UnityEngine;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class TradeOfferInfo
+{
+    public List<string> playerIds;
+    public List<string> playerDisplayNames;
+    public List<string> tradeIds;
+}
 
 public class TradeOffers : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Button[] tradeOfferbuttons;
+    public List<TradeInfo> tradeOffers;
+    public TradeOfferInfo tradeOfferInfo;
+    private int numTradeOffers;
+    
+    // instance
+    public static TradeOffers instance;
+    void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateTradeOffers()
     {
-        
+        DisableAllTradeOfferButtons();
+    }
+
+    void DisableAllTradeOfferButtons()
+    {
+
+    }
+
+    void GetTradeInfo()
+    {
+
+    }
+
+    void UpdateTradeOffersUI()
+    {
+
+    }
+
+    public void OnTradeOfferButton(int tradeIndex)
+    {
+
     }
 }

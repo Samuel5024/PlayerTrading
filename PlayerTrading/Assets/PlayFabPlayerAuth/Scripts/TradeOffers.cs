cb0176e39e5766a0c0ed99f5fc45132965d1d32e
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-12-03 12:00:13 -0700

    Update TradeOffers.cs
    JSONUtility isn't recognized for some reason

--- PlayerTrading/Assets/PlayFabPlayerAuth/Scripts/TradeOffers.cs
+++ PlayerTrading/Assets/PlayFabPlayerAuth/Scripts/TradeOffers.cs
@@ -15,7 +15,7 @@ public class TradeOfferInfo
 
 public class TradeOffers : MonoBehaviour
 {
-    public Button[] tradeOfferbuttons;
+    public Button[] tradeOfferButtons;
     public List<TradeInfo> tradeOffers;
     public TradeOfferInfo tradeOfferInfo;
     private int numTradeOffers;
@@ -30,21 +30,82 @@ public class TradeOffers : MonoBehaviour
     public void UpdateTradeOffers()
     {
         DisableAllTradeOfferButtons();
+
+        ExecuteCloudScriptRequest getTradeOffersRequest = new ExecuteCloudScriptRequest
+        {
+            FunctionName = "GetTradeIDs"
+        };
+
+        PlayFabClientAPI.ExecuteCloudScript(getTradeOffersRequest,
+            result => 
+            {
+                string rawData = result.FunctionResult.ToString();
+                tradeOfferInfo = JSONUtility.From.Json<TradeOfferInfo>(rawData);
+                GetTradeInfo();
+            },
+            error => Debug.Log(error.ErrorMessage)
+        );
     }
 
     void DisableAllTradeOfferButtons()
     {
-
+        foreach(Button button in tradeOfferButtons)
+        {
+            button.gameObject.SetActive(false);
+        }
     }
 
     void GetTradeInfo()
     {
+        numTradeOffers = tradeOfferInfo.playerIds.Count;
+        tradeOffers = new List<TradeInfo>();
 
+        if(numTradeOffers == 0)
+        {
+            UpdateTradeOffersUI();
+        }
+
+        for(int x = 0; x < tradeOfferInfo.playerIds.Count; ++x)
+        {
+            GetTradeStatusRequest tradeStatusRequest = new GetTradeStatusRequest
+            {
+                OfferingPlayerId = tradeOfferInfo.playerIds[x],
+                TradeId = tradeOfferInfo.tradeIds[x]
+            };
+
+            PlayFabClientAPI.GetTradeStatus(tradeStatusRequest,
+                result =>
+                {
+                    tradeOffers.Add(result.Trade);
+
+                    if(tradeOffers.Count == numTradeOffers)
+                    {
+                        UpdateTradeOffersUI();
+                    }
+                },
+                error => Debug.Log(error.ErrorMessage)
+            );
+        }
     }
 
     void UpdateTradeOffersUI()
     {
+        // fill in the UpdateTradeOffersUI function
 
+        for(int x = 00; x < tradeOfferButtons.Length; ++x)
+        {
+            tradeOfferButtons[x].gameObject.SetActive(x < tradeOffers.Count);
+            if(!tradeOfferButtons[x].gameObject.activeInHierarchy)
+            {
+                continue;
+            }
+
+            tradeOfferButtons[x].onClick.RemoveAllListeners();
+
+            int tradeIndex = x;
+            tradeOfferButtons[x].onClick.AddListener( () => OnTradeOfferButton(tradeIndex));
+            tradeOfferButtons[x].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = tradeOfferInfo.playerDisplayNames[x];
+        }
     }
 
     public void OnTradeOfferButton(int tradeIndex)
